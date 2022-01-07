ce34db526216949c2c2beab6dd0d8b27589ede3c
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-01-07 11:11:13 +0000

    add jquery for ourbeers page too toggle text when image is pressed

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
-    $("p").css("color", "green");
-    $(".lager-p").show(0);
+    $(".lager-p").hide(0);
     $(".ipa-p").hide(0);
-    $(".sour-p").show(0);
+    $(".sour-p").hide(0);
+    $(".wheat-p").hide(0);
+    $(".stout-p").hide(0);
 
-    $(".lager").mouseover (function(){
-        $(".lager-p").show(1000);
-    })
-    $(".lager").mouseleave (function(){
-        $(".lager-p").hide(1000);
+  $(".lager").click(function(){
+      $(".lager-p").slideToggle("slow");
+  })
 
-    $(".ipa").mouseover (function(){
-        $(".ipa-p").show(1000);
-    })
-    $(".ipa").mouseleave (function(){
-        $(".ipa-p").hide(1000);
-    
-    $(".sour").mouseover (function(){
-        $(".sour-p").show(1000);
-    })
-    $(".sour").mouseleave (function(){
-        $(".sour-p").hide(1000);
-    })
-    
-    })
-    })
-    });
+  $(".ipa").click(function(){
+    $(".ipa-p").slideToggle("slow");
+  })
+
+  $(".sour").click(function(){
+    $(".sour-p").slideToggle("slow");
+  })
+
+  $(".wheat").click(function(){
+    $(".wheat-p").slideToggle("slow");
+  })
+
+  $(".stout").click(function(){
+    $(".stout-p").slideToggle("slow");
+  })
+
+})
