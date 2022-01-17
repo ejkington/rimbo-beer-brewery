1c1055483a2ce9805cbde1447db37ba672a4756e
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-01-17 13:58:37 +0000

    changed allowed hosts for heroku

--- brewery/settings.py
+++ brewery/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://rimbo-beer-brewery-events.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["rimbo-beer-brewery-events.herokuapp.com/", "localhost"]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
