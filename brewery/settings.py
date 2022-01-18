9a51a266a936c3801ee101e9fd66f2f6f63bcccf
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2022-01-18 12:59:24 +0000

    add mimetypes

--- brewery/settings.py
+++ brewery/settings.py
@@ -13,9 +13,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import mimetypes
 if os.path.isfile("env.py"):
     import env
 
+# mimetype bug fix 
+
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
